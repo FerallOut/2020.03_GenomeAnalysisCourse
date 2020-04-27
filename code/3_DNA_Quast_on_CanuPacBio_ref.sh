fbc2d83125ebaf3f80de283fd3965073a2c27704
FerallOut <mire.balan@gmail.com>
2020-04-27 10:42:35 +0200

    Quast1

--- code/3_DNA_Quast_on_CanuPacBio_ref.sh
+++ code/3_DNA_Quast_on_CanuPacBio_ref.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core			# core/ node
 #SBATCH -n 1			# nr of cores/ nodes
 #SBATCH -t 01:00:00		# time needed (dd-hh:mm:ss)
-#SBATCH -J Quast_Spades_Nano+Ill_ref	# a name for your job
+#SBATCH -J Quast_Canu_PacBio_ref	# a name for your job
 ####################################
 
 # Load modules
@@ -19,12 +19,12 @@ module load quast
 ####################################
 
 # Input sources
-input_Spades_Nano_Illumina=/home/miba8458/2020.03_GenomeAnalysisCourse/scratch/2a_DNA_spades_Nano_Illumina/contigs.fasta
-output_folder_name=/home/miba8458/2020.03_GenomeAnalysisCourse/scratch/3a_DNA_quast_onSpades_Nano_Illumina
+input_Canu_Pacbio=/home/miba8458/2020.03_GenomeAnalysisCourse/scratch/2_DNA_canu_PacBio/canu_pacbio.contigs.fasta
+output_folder_name=/home/miba8458/2020.03_GenomeAnalysisCourse/scratch/3_DNA_quast_onCanu_PacBio
 reference_file=/home/miba8458/2020.03_GenomeAnalysisCourse/data/GCF_000174395.2_ASM17439v2_genomic.fna.gz
 ####################################
 
 # Code to run
-quast.py ${input_Spades_Nano_Illumina} \
+quast.py ${input_Canu_Pacbio} \
 -R ${reference_file} \
 -o ${output_folder_name}
